52574ba2c1f9b5d65f8ced8d152fbb35569ba17a
Teodor Radu <teodor@Teodors-MacBook-Pro.local>
2024-12-11 21:26:34 +0000

    Finish day2

--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
 import {} from './solutions/day1'
+import {} from './solutions/day2'
--- solutions/day2.ts
+++ solutions/day2.ts
@@ -7,15 +7,14 @@ const response = await fetch('https://adventofcode.com/2024/day/2/input', {
 const input = await response.text()
 const lines = input.split('\n').filter(line => line).map(line => line.split(' ').map(num => parseInt(num)))
 
-console.log('Day 2: __________________')
-const safes = lines.reduce((prev, cur) => {
-  const [first, second] = cur
+const checkSafe = (arr: number[]) => {
+  const [first, second] = arr
   if (first === second) {
-    return prev
+    return false
   }
   const increasing = second > first ? true : false
 
-  const safe = cur.reduce((prev, _, ix, arr) => {
+  return arr.reduce((prev, _, ix, arr) => {
     if (ix === arr.length - 1) return prev
 
     const localCur = arr[ix]
@@ -26,8 +25,32 @@ const safes = lines.reduce((prev, cur) => {
     if (delta > 0 && increasing) return false
     return prev
   }, true)
+}
+
+console.log('Day 2: __________________')
+const safes = lines.reduce((prev, cur) => {
+  const safe = checkSafe(cur)
   return safe ? ++prev : prev
 }, 0)
 
 console.log('\tPart 1: ', safes)
 
+const arrayPossibilities = (arr: number[]) => {
+  const possibilities = []
+  for (let i = 0; i < arr.length; i++) {
+    possibilities.push(arr.filter((_, ix) => i !== ix))
+  }
+  return possibilities
+}
+
+const part2Safes = lines.reduce((prev, cur) => {
+  const configs = arrayPossibilities(cur)
+  const safe = configs.reduce((prev, cur) => {
+    const localSafe = checkSafe(cur)
+    return localSafe ? true : prev
+  }, false)
+
+  return safe ? ++prev : prev
+}, 0)
+
+console.log('\tPart 2: ', part2Safes)
